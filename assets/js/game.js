b8074108ad4a89373f1fef3861a869910d15ede5
Seth Uschuk <suschuk24@gmail.com>
2020-08-08 15:46:47 -0500

    basic js commands

---
+++ assets/js/game.js
@@ -0,0 +1,18 @@
+//this creates a function named "fight"
+var playerName = window.prompt("What is your robot's name?");
+
+//this logs the input's name
+console.log(playerName)
+
+//this is a normal string line for making a note or debugging multiple lines of code at once
+console.log("This log is a string, good for leaving yourself a message")
+
+//this will do math and log 20
+console.log(10 + 10)
+
+//This is how to make a note with a string and a variable
+console.log("Our Robot's name is " + playerName)
+function fight() {
+    window.alert("the fight has begun!");
+}
+//fight();
